347c66940a3a41075a2c3c5ef07c2bd8d5630a8c
IgnorantAbyss <treeyden5566@gmail.com>
2024-02-15 12:34:06 +0800

    刪除應忽略文件

--- ignoredir/taining/stripblank.py
+++
@@ -1,8 +0,0 @@
-def stripblank(textlist):
-    t = 0
-    for i in range (len(textlist)):
-        if len(textlist[i]) == 0:
-            t += 1
-    for i in range(t):
-        textlist.remove("")
-    return textlist
